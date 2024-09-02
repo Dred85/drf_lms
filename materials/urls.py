e8db8a2ef5b1ddea96a60ca5e4a14a8acd92b4e4
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-09-02 21:33:34 +0300

    PEP 8 (black)

--- materials/urls.py
+++ materials/urls.py
@@ -1,21 +1,36 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from materials.views import CourseViewSet, LessonCreateApiView, LessonUpdateAPIView, LessonDestroyAPIView, LessonListApiView, LessonRetrieveAPIView
+from materials.views import (
+    CourseViewSet,
+    LessonCreateApiView,
+    LessonUpdateAPIView,
+    LessonDestroyAPIView,
+    LessonListApiView,
+    LessonRetrieveAPIView,
+)
 from materials.apps import MaterialsConfig
 
 
 app_name = MaterialsConfig.name
 
 router = SimpleRouter()
-router.register('', CourseViewSet)
+router.register("", CourseViewSet)
 
 urlpatterns = [
     path("lessons/", LessonListApiView.as_view(), name="lessons_list"),
-    path("lessons/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lessons_retrieve"), # retrieve это PUTCH
+    path(
+        "lessons/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lessons_retrieve"
+    ),  # retrieve это PUTCH
     path("lessons/create/", LessonCreateApiView.as_view(), name="lessons_create"),
-    path("lessons/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lessons_update"), # update это PUT
-    path("lessons/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lessons_delete"),
+    path(
+        "lessons/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lessons_update"
+    ),  # update это PUT
+    path(
+        "lessons/<int:pk>/delete/",
+        LessonDestroyAPIView.as_view(),
+        name="lessons_delete",
+    ),
 ]
 
 urlpatterns += router.urls
--- materials/views.py
+++ materials/views.py
@@ -1,5 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView,
+)
 
 from materials.models import Course, Lesson
 from materials.serializers import CourseSerializer, LessonSerializer
@@ -7,24 +13,30 @@ from materials.serializers import CourseSerializer, LessonSerializer
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()  # выбираем все данные
-    serializer_class = CourseSerializer  # сериалайзер преобразует данные из БД , которые буду выводить
+    serializer_class = (
+        CourseSerializer  # сериалайзер преобразует данные из БД , которые буду выводить
+    )
 
 
 class LessonCreateApiView(CreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
+
 class LessonListApiView(ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
 
 class LessonRetrieveAPIView(RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
+
 class LessonUpdateAPIView(UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
 
 class LessonDestroyAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
