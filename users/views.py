bbbd391b2c76e48d84165b7c538a488be25eb82f
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-09-05 00:24:22 +0300

    Фильтрация

--- users/views.py
+++ users/views.py
@@ -1,13 +1,22 @@
 from rest_framework import viewsets
-from .models import User, Payments
-from .serializers import UserSerializer, PaymentsSerializer
+from django_filters import rest_framework as filters
+from .models import Payments
+from .serializers import PaymentsSerializer
 
+class PaymentsFilter(filters.FilterSet):
+    date_of_payment = filters.DateTimeFilter(field_name='date_of_payment', lookup_expr='exact')
+    course = filters.NumberFilter(field_name='course__id')  # Фильтр по ID курса
+    lesson = filters.NumberFilter(field_name='lesson__id')  # Фильтр по ID урока
+    payment_method_is_cash = filters.BooleanFilter(field_name='payment_method_is_cash')
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
+    class Meta:
+        model = Payments
+        fields = ['date_of_payment', 'course', 'lesson', 'payment_method_is_cash']
 
 class PaymentsViewSet(viewsets.ModelViewSet):
     queryset = Payments.objects.all()
     serializer_class = PaymentsSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = PaymentsFilter
+    ordering_fields = ['date_of_payment']  # Поля, по которым можно сортировать
+    ordering = ['date_of_payment']  # Поле по умолчанию для сортировки
