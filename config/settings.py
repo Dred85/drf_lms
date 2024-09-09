b4d4438123ee8b676492893379a1300ca73d8265
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-09-09 18:09:30 +0300

    добавил simple_jwt и поправил урлы

--- config/settings.py
+++ config/settings.py
@@ -61,6 +61,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
 }
 
 DATABASES = {
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from .apps import UsersConfig
 from .views import PaymentsViewSet, UserViewSet
+
+
+app_name = UsersConfig.name
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet)
@@ -9,4 +14,7 @@ router.register(r"payments", PaymentsViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
