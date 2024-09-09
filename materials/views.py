bf39568244f8e971e6cd15c1e803176139497656
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-09-09 21:56:47 +0300

    установил права для модераторов

--- materials/views.py
+++ materials/views.py
@@ -1,11 +1,13 @@
 from rest_framework.generics import (CreateAPIView, DestroyAPIView,
                                      ListAPIView, RetrieveAPIView,
                                      UpdateAPIView)
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from materials.models import Course, Lesson
 from materials.serializers import (CourseDetailSerializer, CourseSerializer,
                                    LessonSerializer)
+from users.permissions import IsModer
 
 
 class CourseViewSet(ModelViewSet):
@@ -16,25 +18,38 @@ class CourseViewSet(ModelViewSet):
             return CourseDetailSerializer
         return CourseSerializer
 
+    def get_permissions(self):
+        if self.action in ["create", "destroy"]:
+            self.permission_classes = (~IsModer,) # ~ это инверсия, т.е. пользователь должен быть не модератор и не простой пользователь, чтобы мочь создавать и удалять
+        elif self.action in ["update", ]:
+            self.permission_classes = (IsModer,)
+        return super().get_permissions()
+
+
+
 
 class LessonCreateApiView(CreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
 
+
 class LessonListApiView(ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class LessonRetrieveAPIView(RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class LessonUpdateAPIView(UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, IsModer]
 
 
 class LessonDestroyAPIView(DestroyAPIView):
